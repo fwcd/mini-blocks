a9496141499a3f9ffef564897e452a3c04ac2144
fwcd <fwcdmail@gmail.com>
2022-01-06 02:46:03 +0100

    Set up an Xcode project for convenience

---
+++ MiniBlocks/MiniBlocks/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  MiniBlocks
+//
+//  Created by Fredrik on 06.01.22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+        
+    }
+}
