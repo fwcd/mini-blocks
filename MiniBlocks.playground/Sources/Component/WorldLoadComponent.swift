53973e965fb60a3b8a6a2fcebd4fcb637512ae25
fwcd <fwcdmail@gmail.com>
2022-01-07 17:44:05 +0100

    Add loadRadius

--- MiniBlocks.playground/Sources/Component/WorldLoadComponent.swift
+++ MiniBlocks.playground/Sources/Component/WorldLoadComponent.swift
@@ -2,6 +2,9 @@ import GameplayKit
 
 /// Loads chunks from the world model into the SceneKit node.
 class WorldLoadComponent: GKComponent {
+    /// Number of chunks to render in each direction. Note that although we call it a 'radius', a square grid of chunks is loaded.
+    var loadRadius: Int = 2
+    
     private var world: World? {
         get { entity?.component(ofType: WorldComponent.self)?.world }
         set { entity?.component(ofType: WorldComponent.self)?.world = newValue! }
