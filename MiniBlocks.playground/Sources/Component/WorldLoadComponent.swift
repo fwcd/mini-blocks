53e8547986b6d882cb7dc3b29fbc4fa230b2d039
fwcd <fwcdmail@gmail.com>
2022-01-07 16:41:30 +0100

    Add WorldLoadComponent and WorldRetainComponent stubs

---
+++ MiniBlocks.playground/Sources/Component/WorldLoadComponent.swift
@@ -0,0 +1,6 @@
+import GameplayKit
+
+/// Loads chunks from the world model into the SceneKit node.
+class WorldLoadComponent: GKComponent {
+    
+}
---
+++ MiniBlocks.playground/Sources/Component/WorldRetainComponent.swift
@@ -0,0 +1,6 @@
+import GameplayKit
+
+/// Lets the associated node keep the world at the corresponding 
+class WorldRetainComponent: GKComponent {
+    
+}
