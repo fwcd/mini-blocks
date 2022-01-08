32231910fbbd43c83181ddf6824d55af04a362c1
fwcd <fwcdmail@gmail.com>
2022-01-08 15:39:49 +0100

    Slightly update naming

--- MiniBlocks.playground/Sources/World/NatureWorldGenerator.swift
+++ MiniBlocks.playground/Sources/World/NatureWorldGenerator.swift
@@ -2,22 +2,25 @@ import GameplayKit
 
 /// Generates a world with realistic terrain and natural features.
 struct NatureWorldGenerator: WorldGenerator {
-    private let noise: GKNoise
+    private let heightNoise: GKNoise
+    
     var amplitude: Float = 8
     var scale: Float = 80
     
     init(seed: String) {
-        noise = GKNoise(GKPerlinNoiseSource(
+        let noiseSeed: Int32 = seed.utf8.reduce(0) { ($0 << 1) ^ Int32($1) }
+        
+        heightNoise = GKNoise(GKPerlinNoiseSource(
             frequency: 1,
             octaveCount: 10,
             persistence: 0.8,
             lacunarity: 1.2,
-            seed: seed.utf8.reduce(0) { ($0 << 1) ^ Int32($1) }
+            seed: noiseSeed
         ))
     }
     
     func generate(at pos: GridPos2) -> Strip {
-        let y = Int(amplitude * noise.value(atPosition: vector_float2(x: Float(pos.x) / scale, y: Float(pos.z) / scale)))
+        let y = Int(amplitude * heightNoise.value(atPosition: vector_float2(x: Float(pos.x) / scale, y: Float(pos.z) / scale)))
         return Strip(blocks: [y: Block(type: .grass)])
     }
 }
