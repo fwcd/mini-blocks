38418ad845e564ff285d14cb073a91056250f1fa
fwcd <fwcdmail@gmail.com>
2022-01-10 11:55:47 +0100

    Move node factories that crate sprite/scene nodes into group

--- MiniBlocks.playground/Sources/Entity/CrosshairHUDEntity.swift
+++ MiniBlocks.playground/Sources/Entity/CrosshairHUDEntity.swift
@@ -1,23 +1,9 @@
 import SpriteKit
 import GameplayKit
 
-func makePartNode(size: CGSize) -> SKNode {
-    let node = SKShapeNode(rect: CGRect(center: CGPoint(x: 0, y: 0), size: size))
-    node.fillColor = .white
-    node.strokeColor = .clear
-    return node
-}
-
-func makeCrosshairNode(size: CGFloat, thickness: CGFloat) -> SKNode {
-    let node = SKNode()
-    node.addChild(makePartNode(size: CGSize(width: size, height: thickness)))
-    node.addChild(makePartNode(size: CGSize(width: thickness, height: size)))
-    return node
-}
-
 func makeCrosshairHUDEntity(size: CGFloat = 20, thickness: CGFloat = 2, in frame: CGRect) -> GKEntity {
     // Create node
-    let node = makeCrosshairNode(size: size, thickness: thickness)
+    let node = makeCrosshairHUDNode(size: size, thickness: thickness)
     node.position = CGPoint(x: frame.midX, y: frame.midY)
     
     // Create entity
---
+++ MiniBlocks.playground/Sources/Node/CrosshairHUDNode.swift
@@ -0,0 +1,16 @@
+import SpriteKit
+import GameplayKit
+
+private func makePartNode(size: CGSize) -> SKNode {
+    let node = SKShapeNode(rect: CGRect(center: CGPoint(x: 0, y: 0), size: size))
+    node.fillColor = .white
+    node.strokeColor = .clear
+    return node
+}
+
+func makeCrosshairHUDNode(size: CGFloat, thickness: CGFloat) -> SKNode {
+    let node = SKNode()
+    node.addChild(makePartNode(size: CGSize(width: size, height: thickness)))
+    node.addChild(makePartNode(size: CGSize(width: thickness, height: size)))
+    return node
+}
