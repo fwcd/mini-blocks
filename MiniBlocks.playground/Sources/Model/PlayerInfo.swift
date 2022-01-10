2fd2038df73e91f8de5ef7cc1fd8b1d240ddfb86
fwcd <fwcdmail@gmail.com>
2022-01-10 12:35:25 +0100

    Use 8 hotbar slots again

--- MiniBlocks.playground/Sources/Model/PlayerInfo.swift
+++ MiniBlocks.playground/Sources/Model/PlayerInfo.swift
@@ -3,9 +3,9 @@ import SceneKit
 /// Information about a player.
 struct PlayerInfo: Codable, Hashable {
     /// The player's main inventory.
-    var inventory: Inventory = Inventory(slotCount: 18)
+    var inventory: Inventory = Inventory(slotCount: 24)
     /// The player's hotbar.
-    var hotbar: Inventory = Inventory(slotCount: 6)
+    var hotbar: Inventory = Inventory(slotCount: 8)
     
     // TODO: Store player position?
 }
