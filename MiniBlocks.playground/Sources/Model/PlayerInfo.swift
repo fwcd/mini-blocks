f98031814d68ee486e29c6abfb1e1c372a7c6c63
fwcd <fwcdmail@gmail.com>
2022-01-10 12:32:47 +0100

    Make hotbar slightly smaller

--- MiniBlocks.playground/Sources/Model/PlayerInfo.swift
+++ MiniBlocks.playground/Sources/Model/PlayerInfo.swift
@@ -3,9 +3,9 @@ import SceneKit
 /// Information about a player.
 struct PlayerInfo: Codable, Hashable {
     /// The player's main inventory.
-    var inventory: Inventory = Inventory(slotCount: 24)
+    var inventory: Inventory = Inventory(slotCount: 18)
     /// The player's hotbar.
-    var hotbar: Inventory = Inventory(slotCount: 8)
+    var hotbar: Inventory = Inventory(slotCount: 6)
     
     // TODO: Store player position?
 }
