9e05ae921a549523d276bbb154e62f2d6d55b4e6
fwcd <fwcdmail@gmail.com>
2022-01-10 10:42:56 +0100

    Move crosshair HUD into separate subclass

--- MiniBlocks.playground/Sources/Entity/CrosshairHUDEntity.swift
+++ MiniBlocks.playground/Sources/Entity/CrosshairHUDEntity.swift
@@ -1,19 +1,10 @@
 import SpriteKit
 import GameplayKit
 
-private func makeCrosshairPart(size: CGSize) -> SKNode {
-    let node = SKShapeNode(rect: CGRect(center: CGPoint(x: 0, y: 0), size: size))
-    node.fillColor = .white
-    node.strokeColor = .clear
-    return node
-}
-
 func makeCrosshairHUDEntity(size: CGFloat = 20, thickness: CGFloat = 2, at position: CGPoint) -> GKEntity {
     // Create node
-    let node = SKNode()
+    let node = CrosshairHUDNode()
     node.position = position
-    node.addChild(makeCrosshairPart(size: CGSize(width: size, height: thickness)))
-    node.addChild(makeCrosshairPart(size: CGSize(width: thickness, height: size)))
     
     // Create entity
     let entity = GKEntity()
