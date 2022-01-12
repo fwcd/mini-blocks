53db97ece247ba06999d0148b6dd37443406b545
fwcd <fwcdmail@gmail.com>
2022-01-12 04:05:11 +0100

    Remove debug logging of hits/misses again

--- MiniBlocks.playground/Sources/Utils/CachedNoise.swift
+++ MiniBlocks.playground/Sources/Utils/CachedNoise.swift
@@ -6,23 +6,15 @@ struct CachedNoise {
     private let scale: Float
     @Box private var cache = FIFOCache<GridPos2, Float>(capacity: 64)
     
-    @Box private var hits: Int = 0
-    @Box private var misses: Int = 0
-    
     init(noise: GKNoise, scale: Float) {
         self.noise = noise
         self.scale = scale
     }
     
     func value(at pos: GridPos2) -> Float {
-        if (hits + misses) % 10000 == 0 {
-            print("Hits: \(hits), misses: \(misses), ratio: \(Float(hits) / Float(misses))")
-        }
         if let cachedValue = cache[pos] {
-            hits += 1
             return cachedValue
         } else {
-            misses += 1
             let newValue = noise.value(atPosition: vectorOf(pos: pos))
             cache.insert(pos, newValue)
             return newValue
