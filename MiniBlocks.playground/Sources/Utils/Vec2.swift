6aa4df80fd0831febae824be9567e74ab5da7f29
fwcd <fwcdmail@gmail.com>
2022-01-17 11:21:36 +0100

    Add Vec2 and Vec3 (again)

--- MiniBlocks.playground/Sources/Utils/Vec2.swift
+++ MiniBlocks.playground/Sources/Utils/Vec2.swift
@@ -1,1 +1,9 @@
-
+struct Vec2: Hashable, Codable, Vec2Protocol {
+    var x: Double
+    var z: Double
+    
+    init(x: Double = 0, z: Double = 0) {
+        self.x = x
+        self.z = z
+    }
+}
--- MiniBlocks.playground/Sources/Utils/Vec3.swift
+++ MiniBlocks.playground/Sources/Utils/Vec3.swift
@@ -1,1 +1,11 @@
-
+struct Vec3: Hashable, Codable, Vec3Protocol {
+    var x: Double
+    var y: Double
+    var z: Double
+    
+    init(x: Double = 0, y: Double = 0, z: Double = 0) {
+        self.x = x
+        self.y = y
+        self.z = z
+    }
+}
