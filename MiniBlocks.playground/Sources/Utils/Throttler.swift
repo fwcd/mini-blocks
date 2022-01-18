2fae0c1e53881ce4adef5757cd89939258ad504b
fwcd <fwcdmail@gmail.com>
2022-01-18 01:36:32 +0100

    Rename variable for consistency

--- MiniBlocks.playground/Sources/Utils/Throttler.swift
+++ MiniBlocks.playground/Sources/Utils/Throttler.swift
@@ -3,17 +3,17 @@ import Foundation
 /// A facility that only executes an action if it hasn't executed within the last interval.
 struct Throttler {
     let interval: TimeInterval
-    private var lastRun: TimeInterval = 0
+    private var timeSinceLastRun: TimeInterval = 0
     
     init(interval: TimeInterval) {
         self.interval = interval
     }
     
     mutating func run(deltaTime: TimeInterval, action: () -> Void, orElse alternativeAction: () -> Void = {}) {
-        lastRun += deltaTime
-        if lastRun > interval {
+        timeSinceLastRun += deltaTime
+        if timeSinceLastRun > interval {
             action()
-            lastRun = 0
+            timeSinceLastRun = 0
         } else {
             alternativeAction()
         }
