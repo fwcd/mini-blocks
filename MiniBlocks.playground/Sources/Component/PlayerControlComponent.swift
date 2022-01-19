34f0096657099b1d7e338295327cbac1db18baca
fwcd <fwcdmail@gmail.com>
2022-01-19 11:20:40 +0100

    Tweak jump speed

--- MiniBlocks.playground/Sources/Component/PlayerControlComponent.swift
+++ MiniBlocks.playground/Sources/Component/PlayerControlComponent.swift
@@ -12,7 +12,7 @@ class PlayerControlComponent: GKComponent {
     private var baseSpeed: Double = 0.5
     private var pitchSpeed: SceneFloat = 0.4
     private var yawSpeed: SceneFloat = 0.3
-    private var jumpSpeed: Double = 1.2
+    private var jumpSpeed: Double = 1
     private var sprintFactor: Double = 1.8
     private var maxCollisionIterations: Int = 5
     private var pitchRange: ClosedRange<SceneFloat> = -piHalf...piHalf
