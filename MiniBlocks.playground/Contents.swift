38dd5b3aac05147f886c68b05ae36966b64e7d82
fwcd <fwcdmail@gmail.com>
2022-01-19 11:52:10 +0100

    Update initialization in playground

--- MiniBlocks.playground/Contents.swift
+++ MiniBlocks.playground/Contents.swift
@@ -15,13 +15,13 @@ PlaygroundPage.current.liveView = MiniBlocksViewController(
     sceneFrame: CGRect(x: 0, y: 0, width: 640, height: 480),
     // The world generator to use, e.g. .empty, .flat, .wavyHills, .nature or .ocean (Note that .ocean is quite resource-intensive though!).
     worldGenerator: .nature(seed: "Hello WWDC 2022!"),
+    // The game mode determines the abilities of the player. In .survival mode the player is affected by gravity whereas .creative mode lets the player fly freely (ascend using space and descend using control).
+    gameMode: .survival,
     // How many chunks to render in each direction. Large values may impact performance and memory usage. Note that this does not affect SceneKit's far plane, so the actual number of visible chunks may be lower.
     renderDistance: 8,
     // Ambient occlusion renders shadows in block corners for a more realistic look (which is, however, computationally more expensive). Disable to get better performance.
     ambientOcclusionEnabled: true,
-    // In debug mode gravity for the player is disabled, the camera can be moved freely and scene statistics will be shown.
-    debugModeEnabled: false,
-    // The camera interaction mode. Only used if debug mode is enabled.
-    debugInteractionMode: .fly
+    // Displays some SceneKit statistics such as frames-per-second when enabled.
+    debugStatsShown: false
 )
 
