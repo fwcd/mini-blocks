0a4e7fff678355316aeb9b553f0d9362f8fa31c6
fwcd <fwcdmail@gmail.com>
2022-03-29 04:51:39 +0200

    Add doc comment on visibleWhenCaptured

--- MiniBlocks.playground/Sources/Component/MouseCaptureVisibilityComponent.swift
+++ MiniBlocks.playground/Sources/Component/MouseCaptureVisibilityComponent.swift
@@ -2,6 +2,8 @@ import GameplayKit
 
 /// Makes the components visibility dependent on whether the mouse is captured.
 class MouseCaptureVisibilityComponent: GKComponent {
+    /// If true, the associated node will be visible if and only if the mouse is captured.
+    /// If false, the associated node will be visible if and only if it is not captured.
     private let visibleWhenCaptured: Bool
     
     private var node: SKNode? {
