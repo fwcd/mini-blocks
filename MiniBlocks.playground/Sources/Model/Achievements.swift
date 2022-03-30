4127f9eb3f097ce7cb2a0fb37a9c342b453e521c
fwcd <fwcdmail@gmail.com>
2022-03-30 23:51:20 +0200

    Conform Achievements to Sequence

--- MiniBlocks.playground/Sources/Model/Achievements.swift
+++ MiniBlocks.playground/Sources/Model/Achievements.swift
@@ -1,25 +1,37 @@
 /// Represents in-game progress milestones by the player.
-struct Achievements: OptionSet {
+struct Achievements: OptionSet, Sequence {
     let rawValue: UInt64
-    
-    var descriptions: [String] {
-        (0..<UInt64(RawValue.bitWidth))
-            .filter { (rawValue >> $0) & 1 == 1 }
-            .compactMap {
-                switch Achievements(rawValue: $0) {
-                case .moveAround:
-                    return "Move around using your WASD keys."
-                case .jump:
-                    return "Press SPACE to jump."
-                case .sprint:
-                    return "Hold SHIFT while moving around with WASD to sprint."
-                default:
-                    return nil
-                }
-            }
-    }
     
     static let moveAround = Self(rawValue: 1 << 0)
     static let jump = Self(rawValue: 1 << 1)
     static let sprint = Self(rawValue: 1 << 2)
+    
+    var description: String? {
+        switch self {
+        case .moveAround:
+            return "Move around using your WASD keys."
+        case .jump:
+            return "Press SPACE to jump."
+        case .sprint:
+            return "Hold SHIFT while moving around with WASD to sprint."
+        default:
+            return nil
+        }
+    }
+    
+    func makeIterator() -> Iterator {
+        Iterator(rawValue: rawValue)
+    }
+    
+    struct Iterator: IteratorProtocol {
+        let rawValue: RawValue
+        var i: RawValue = 0
+        
+        mutating func next() -> Achievements? {
+            guard i < RawValue.bitWidth else { return nil }
+            let bit = (rawValue >> i) & 1
+            i += 1
+            return Achievements(rawValue: bit)
+        }
+    }
 }
