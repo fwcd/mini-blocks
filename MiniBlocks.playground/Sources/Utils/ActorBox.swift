debc9880dd7b0ac90308c30d70af0667f478df70
fwcd <fwcdmail@gmail.com>
2022-04-01 21:08:14 +0200

    Define a global WorldActor

--- MiniBlocks.playground/Sources/Utils/ActorBox.swift
+++
@@ -1,8 +0,0 @@
-/// A wrapper that adds reference semantics/shared ownership and isolation to a value.
-actor ActorBox<Wrapped> {
-    private var wrappedValue: Wrapped
-    
-    init(wrappedValue: Wrapped) {
-        self.wrappedValue = wrappedValue
-    }
-}
