5352ff2fc71107aa3713fc87a86f59dd5aaeaad2
fwcd <fwcdmail@gmail.com>
2022-04-22 15:25:24 +0200

    Fix watch app icon

--- MiniBlocks.swiftpm/App/MiniBlocksApp.swift
+++ MiniBlocks.swiftpm/App/MiniBlocksApp.swift
@@ -1,13 +1,19 @@
 import SwiftUI
 import SceneKit
 
-private let viewController = MiniBlocksViewController()
+private let viewController = MiniBlocksViewController(
+    worldGenerator: .wavyHills
+)
 
 @main
 struct MiniBlocksApp: App {
     var body: some Scene {
         WindowGroup {
-            SceneView(scene: viewController.scene)
+            SceneView(
+                scene: viewController.scene,
+                antialiasingMode: .none,
+                delegate: viewController
+            )
         }
     }
 }
