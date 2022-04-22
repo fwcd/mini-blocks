269d395eed2a1a21628794b2007dd768f0422733
fwcd <fwcdmail@gmail.com>
2022-04-22 13:06:27 +0200

    Add watchOS target

---
+++ MiniBlocks/MiniBlocksToWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniBlocksToWatch WatchKit Extension
+//
+//  Created by Fredrik on 22.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniBlocks/MiniBlocksToWatch WatchKit Extension/MiniBlocksApp.swift
@@ -0,0 +1,19 @@
+//
+//  MiniBlocksApp.swift
+//  MiniBlocksToWatch WatchKit Extension
+//
+//  Created by Fredrik on 22.04.22.
+//
+
+import SwiftUI
+
+@main
+struct MiniBlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
